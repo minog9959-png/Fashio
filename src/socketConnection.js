03fc16a1eaef81b15c603274aaecbc665da19bc3
minog9959-png <minog9959@gmail.com>
2026-08-27 17:42:13 +0500

    errorhandle

--- src/socketConnection.js
+++ src/socketConnection.js
@@ -7,14 +7,23 @@
 // export default socket;
 
 
+// import { io } from "socket.io-client";
+
+// let socket = null;
+
+// if (import.meta.env.MODE !== "production") {
+//   socket = io("http://localhost:8000", {
+//     transports: ["websocket"],
+//   });
+// }
+
+// export default socket;
+
 import { io } from "socket.io-client";
 
-let socket = null;
-
-if (import.meta.env.MODE !== "production") {
-  socket = io("http://localhost:8000", {
-    transports: ["websocket"],
-  });
-}
+const socket =
+  import.meta.env.DEV
+    ? io("http://localhost:8000")
+    : null;
 
 export default socket;
