9b1603048b48c654264dbba338cbfba2991d2c98
minog9959-png <minog9959@gmail.com>
2026-08-28 12:29:39 +0500

    minepro

--- src/socketConnection.js
+++ src/socketConnection.js
@@ -19,15 +19,27 @@
 
 // export default socket;
 
+// import { io } from "socket.io-client";
+
+// const socket = io(
+//   import.meta.env.DEV
+//     ? "http://localhost:8000"
+//     : import.meta.env.VITE_API_URL.replace("/api", ""),
+//   {
+//     transports: ["websocket"],
+//   }
+// );
+
+// export default socket;
+
 import { io } from "socket.io-client";
 
-const socket = io(
-  import.meta.env.DEV
-    ? "http://localhost:8000"
-    : import.meta.env.VITE_API_URL.replace("/api", ""),
-  {
+let socket = null;
+
+if (import.meta.env.DEV) {
+  socket = io("http://localhost:8000", {
     transports: ["websocket"],
-  }
-);
+  });
+}
 
 export default socket;
