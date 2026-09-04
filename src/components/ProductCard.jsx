fc1722f97565f380a3c54fb56108c2e5201ac335
minog9959-png <minog9959@gmail.com>
2026-09-04 20:51:21 +0500

    seo

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,38 +1,49 @@
 import { Link } from "react-router-dom";
 
-const Products = ({ productItem  }) => {
+const Products = ({ productItem }) => {
   return (
     <Link to={`/product/${productItem._id}`}>
-    <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition duration-300">
-      
-      {/* Product Image */}
-      <img
+      <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition duration-300">
+
+        {/* Product Image */}
+        {/* <img
         src={productItem.image}
         alt={productItem.title}
         className="w-full h-64 object-cover"
-      />
+      /> */}
 
-      {/* Product Info */}
-      <div className="p-4">
+        {/* Product Image */}
+        <img
+          src={productItem.image}
+          alt={productItem.title}
+          width={355}
+          height={256}
+          loading="lazy"
+          decoding="async"
+          className="w-full h-64 object-cover"
+        />
 
-        <h2 className="text-lg font-semibold">
-          {productItem.title}
-        </h2>
+        {/* Product Info */}
+        <div className="p-4">
 
-        <p className="text-gray-500 mt-2">
-          {productItem.category?.name}
-        </p>
+          <h2 className="text-lg font-semibold">
+            {productItem.title}
+          </h2>
 
-        <p className="text-2xl font-bold text-red-500 mt-3">
-          ${productItem.price}
-        </p>
+          <p className="text-gray-500 mt-2">
+            {productItem.category?.name}
+          </p>
 
-        <button className="w-full mt-4 bg-black text-white py-2 rounded hover:bg-gray-800 transition">
-          Add To Cart
-        </button>
+          <p className="text-2xl font-bold text-red-500 mt-3">
+            ${productItem.price}
+          </p>
 
+          <button className="w-full mt-4 bg-black text-white py-2 rounded hover:bg-gray-800 transition">
+            Add To Cart
+          </button>
+
+        </div>
       </div>
-    </div>
     </Link>
   );
 };
